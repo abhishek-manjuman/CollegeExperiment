4c72ac6ade8e3a696774545599723dcec4fafc1e
Manjuman Abhishek <maurya.abhishek265@gmail.com>
2018-11-05 22:11:12 +0530

    no message

--- app/src/main/java/com/dacode/collegeexperiment/MoviesActivity.java
+++ app/src/main/java/com/dacode/collegeexperiment/MoviesActivity.java
@@ -2,12 +2,36 @@ package com.dacode.collegeexperiment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MoviesActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        radioGroup = findViewById(R.id.radio_group);
+        button = findViewById(R.id.btn_movie_next);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int radioId = radioGroup.getCheckedRadioButtonId();
+
+                radioButton = findViewById(radioId);
+
+                Toast.makeText(MoviesActivity.this,radioButton.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
