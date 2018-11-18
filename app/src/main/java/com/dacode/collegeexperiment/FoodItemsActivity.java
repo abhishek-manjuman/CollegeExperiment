55724ef85fdd4b3e79f93507be05f9411efbf5ef
Manjuman Abhishek <maurya.abhishek265@gmail.com>
2018-11-18 16:40:53 +0530

    no message

---
+++ app/src/main/java/com/dacode/collegeexperiment/FoodItemsActivity.java
@@ -0,0 +1,15 @@
+package com.dacode.collegeexperiment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodItemsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_items);
+    }
+}
